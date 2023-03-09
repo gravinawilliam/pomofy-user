09824a99ec5e377a7fed2d7366b295112b11640a
William Gravina <dev.gravinawilliam@gmail.com>
2023-03-09 15:40:27 -0300

    feat: created status error

---
+++ src/domain/entities/errors/_shared/status.error.ts
@@ -0,0 +1,8 @@
+export enum StatusError {
+  NOT_FOUND = 'not_found',
+  INVALID = 'invalid',
+  REPOSITORY_ERROR = 'repository_error',
+  PROVIDER_ERROR = 'provider_error',
+  CONFLICT = 'conflict',
+  UNAUTHORIZED = 'unauthorized'
+}
