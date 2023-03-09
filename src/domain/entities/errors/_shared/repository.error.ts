acad695c2babca3c87f4b0f3c4fc3c23f756f50d
William Gravina <dev.gravinawilliam@gmail.com>
2023-03-09 15:42:43 -0300

    feat: created error for repository

---
+++ src/domain/entities/errors/_shared/repository.error.ts
@@ -0,0 +1,48 @@
+import { StatusError } from './status.error';
+
+type ParametersConstructorDTO = {
+  error?: Error;
+  repository: {
+    name: RepositoryNames;
+    method: UsersRepositoryMethods | TokensForgotPasswordRepositoryMethods;
+    externalName?: string;
+  };
+};
+
+export enum RepositoryNames {
+  USERS = 'users',
+  TOKENS_FORGOT_PASSWORD = 'tokens forgot password'
+}
+
+export enum UsersRepositoryMethods {
+  FIND_BY_EMAIL = 'find by email',
+  SAVE = 'save',
+  SAVE_WITH_FACEBOOK_ACCOUNT = 'save with facebook account',
+  SAVE_WITH_GOOGLE_ACCOUNT = 'save with google account',
+  UPDATE = 'update'
+}
+
+export enum TokensForgotPasswordRepositoryMethods {
+  SAVE = 'save'
+}
+
+export class RepositoryError {
+  readonly status: StatusError;
+
+  readonly message: string;
+
+  readonly name: 'RepositoryError';
+
+  readonly error?: Error;
+
+  constructor(parameters: ParametersConstructorDTO) {
+    this.name = 'RepositoryError';
+    this.message = `Error in ${parameters.repository.name} repository in ${parameters.repository.method} method.${
+      parameters.repository.externalName === undefined
+        ? ''
+        : ` Error in external lib name: ${parameters.repository.externalName}.`
+    }`;
+    this.status = StatusError.PROVIDER_ERROR;
+    this.error = parameters.error;
+  }
+}
